791621909e05486b4b7236d05937e1e0bdcfe673
李廷礼 <litingxian@live.cn>
2023-10-27 02:43:06 +0800

    乱码处理

--- Com.GleekFramework.Models/Models/WeatherForecasts/WeatherForecastModel.cs
+++ Com.GleekFramework.Models/Models/WeatherForecasts/WeatherForecastModel.cs
@@ -10,25 +10,25 @@ namespace Com.GleekFramework.Models
     public class WeatherForecastModel
     {
         /// <summary>
-        /// ʱ��
+        /// 时间
         /// </summary>
         [JsonProperty("date"), JsonPropertyName("date")]
         public DateTime Date { get; set; }
 
         /// <summary>
-        /// �¶�(���϶�)
+        /// 摄氏度
         /// </summary>
         [JsonProperty("temperature_c"), JsonPropertyName("temperature_c")]
         public int TemperatureC { get; set; }
 
         /// <summary>
-        /// ����
+        /// 描述
         /// </summary>
         [JsonProperty("summary"), JsonPropertyName("summary")]
         public string Summary { get; set; }
 
         /// <summary>
-        /// �¶�(���϶�)
+        /// 华氏度
         /// </summary>
         [JsonProperty("temperature_f"), JsonPropertyName("temperature_f")]
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
