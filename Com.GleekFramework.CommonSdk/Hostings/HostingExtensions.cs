f67b6249ae1b1e56c3b84b95154e08d1b14b0e8a
李廷礼 <litingxian@live.cn>
2023-10-29 19:51:32 +0800

    代码优化

--- Com.GleekFramework.CommonSdk/Hostings/HostingExtensions.cs
+++ Com.GleekFramework.CommonSdk/Hostings/HostingExtensions.cs
@@ -10,14 +10,14 @@ namespace Com.GleekFramework.CommonSdk
         /// <summary>
         /// 启用信号两开关
         /// </summary>
-        /// <param name="host"></param>
+        /// <param name="builder"></param>
         /// <param name="minProcessorCount">最小能够处理请求的线程数(初始值)</param>
         /// <param name="maxProcessorCount">最大能够处理请求的线程数量(上限值)</param>
         /// <returns></returns>
-        public static IHost UseSemaphoreSwitch(this IHost host, int minProcessorCount, int maxProcessorCount)
+        public static IHostBuilder UseSemaphoreSwitch(this IHostBuilder builder, int minProcessorCount, int maxProcessorCount)
         {
             SemaphoreProvider.SetSemaphoreSwitch(minProcessorCount, maxProcessorCount);
-            return host;
+            return builder;
         }
     }
 }
