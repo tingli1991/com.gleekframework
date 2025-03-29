1aa77090db68150fd65d3b3c2d025e5248caa7d8
李廷礼 <litingxian@live.cn>
2025-03-29 23:32:47 +0800

    feat:增加允许用户授权范围特性

---
+++ Com.GleekFramework.AttributeSdk/Attributes/AllowAuthorizeAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace Com.GleekFramework.AttributeSdk
+{
+    /// <summary>
+    /// 允许用户授权范围特性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class AllowAuthorizeAttribute : Attribute
+    {
+        /// <summary>
+        /// 是否验证令牌过期
+        /// </summary>
+        public bool VerifyToken { get; set; } = true;
+
+        /// <summary>
+        /// 是否验证用户状态(只有验证令牌的的同时才会验证状态)
+        /// </summary>
+        public bool VerifyStatus { get; set; } = true;
+    }
+}
---
+++ Com.GleekFramework.AttributeSdk/Extensions/AllowAuthorizeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Com.GleekFramework.CommonSdk;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Com.GleekFramework.AttributeSdk
+{
+    /// <summary>
+    /// 允许用户授权范围拓展类
+    /// </summary>
+    public static class AllowAuthorizeExtensions
+    {
+        /// <summary>
+        /// 获取允许用户授权范围特性
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static AllowAuthorizeAttribute GetAllowAuthorizeAttribute(this AuthorizationFilterContext context)
+        {
+            var actionDescriptor = context.ActionDescriptor as ControllerActionDescriptor;//获取当前Action和Controller的描述信息
+            var methodAllowAuthorizeAttribute = actionDescriptor.MethodInfo.GetCustomAttribute<AllowAuthorizeAttribute>();//方法上的匿名函数特性
+            var controllerAllowAuthorizeAttribute = actionDescriptor.ControllerTypeInfo.GetCustomAttribute<AllowAuthorizeAttribute>();//控制器上的匿名函数特性
+            return methodAllowAuthorizeAttribute ?? controllerAllowAuthorizeAttribute ?? new AllowAuthorizeAttribute();
+        }
+    }
+}
