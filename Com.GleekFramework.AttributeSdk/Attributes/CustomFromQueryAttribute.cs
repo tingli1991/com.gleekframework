90c43ef566ba0d4720c4e7e612bd436486e015b1
李廷礼 <litingxian@live.cn>
2025-04-28 23:24:17 +0800

    新增自定义验证属性和XML文档说明
    在 `Com.GleekFramework.AttributeSdk.xml` 文件中，添加了 `CustomFromQueryAttribute` 和 `CustomRequiredAttribute` 的相关信息，包括摘要和构造函数参数说明。
    
    新增 `CustomFromQueryAttribute` 类，继承自 `ValidationAttribute`，实现了多个接口，用于从查询字符串中获取必填参数。
    
    新增 `CustomRequiredAttribute` 类，继承自 `ValidationAttribute`，用于实现必填验证，并定义了构造函数以设置验证失败时的错误消息。

---
+++ Com.GleekFramework.AttributeSdk/Attributes/CustomFromQueryAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace Com.GleekFramework.AttributeSdk.Attributes
+{
+    /// <summary>
+    /// 从查询字符串中获取参数，并且该参数为必填项
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+    class CustomFromQueryAttribute : ValidationAttribute, IBindingSourceMetadata, IModelNameProvider, IFromQueryMetadata
+    {
+        /// <summary>
+        /// 属性名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 绑定资源
+        /// </summary>
+        public BindingSource BindingSource { get; }
+
+        /// <summary>
+        /// 验证失败时的错误消息
+        /// </summary>
+        /// <param name="errorEnum"></param>
+        public CustomFromQueryAttribute(Enum errorEnum) : base() => ErrorMessage = nameof(errorEnum);
+    }
+}
---
+++ Com.GleekFramework.AttributeSdk/Attributes/CustomRequiredAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Com.GleekFramework.AttributeSdk
+{
+    /// <summary>
+    /// 必填验证
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class CustomRequiredAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// 验证失败时的错误消息
+        /// </summary>
+        /// <param name="errorEnum"></param>
+        public CustomRequiredAttribute(Enum errorEnum) : base() => ErrorMessage = nameof(errorEnum);
+    }
+}
