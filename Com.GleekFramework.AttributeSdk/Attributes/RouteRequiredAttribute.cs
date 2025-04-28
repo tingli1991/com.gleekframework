ed07bbd53790083b7eaf5a3cd254e8942e458cf2
李廷礼 <litingxian@live.cn>
2025-04-29 00:17:44 +0800

    添加路由必填验证属性及相关文档
    在 `QueryRequiredAttribute.cs` 中添加了对 `Microsoft.AspNetCore.Mvc.ModelBinding` 的引用，并更新了类注释。
    在 `Com.GleekFramework.AttributeSdk.xml` 中更新了 `QueryRequiredAttribute` 的文档注释，并新增了 `RouteRequiredAttribute` 的文档。
    在 `RouteRequiredAttribute.cs` 中新增了 `RouteRequiredAttribute` 类，提供路由参数的必填验证功能。

--- Com.GleekFramework.AttributeSdk/Attributes/RouteRequiredAttribute.cs
+++ Com.GleekFramework.AttributeSdk/Attributes/RouteRequiredAttribute.cs
@@ -5,10 +5,10 @@ using System.ComponentModel.DataAnnotations;
 namespace Com.GleekFramework.AttributeSdk
 {
     /// <summary>  
-    /// Query必填验证
+    /// 路由必填验证
     /// </summary>  
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
-    public class QueryRequiredAttribute : ValidationAttribute, IBindingSourceMetadata, IModelNameProvider, IFromQueryMetadata
+    public class RouteRequiredAttribute : ValidationAttribute, IBindingSourceMetadata, IModelNameProvider, IFromRouteMetadata
     {
         /// <summary>  
         /// 属性名称  
@@ -21,11 +21,11 @@ namespace Com.GleekFramework.AttributeSdk
         public BindingSource BindingSource { get; }
 
         /// <summary>  
-        /// 验证失败时的错误消息  
+        /// 验证失败时的错误消息
         /// </summary>  
         /// <param name="name"></param>  
         /// <param name="errorMessage"></param>  
-        public QueryRequiredAttribute(string name, object errorMessage) : base()
+        public RouteRequiredAttribute(string name, object errorMessage) : base()
         {
             Name = name;
             ErrorMessage = errorMessage?.ToString();
