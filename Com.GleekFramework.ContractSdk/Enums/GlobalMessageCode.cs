e9bdd52acffab0ba7c8e1c16641beb537b2adb38
李廷礼 <litingxian@live.cn>
2025-05-11 20:33:49 +0800

    更新属性和错误代码描述信息
    在 `Com.GleekFramework.AttributeSdk.xml` 中新增了 `AllowAuthorizeAttribute` 类的 `_verifyStatus` 字段及其描述信息。
    
    在 `Com.GleekFramework.ContractSdk.xml` 中，修改了多个字段的名称和描述，包括：
    - 更新 `GlobalMessageCode.FAIL` 和 `GlobalMessageCode.SUCCESS` 的描述。
    - 更名 `TOKEN_INVALID` 为 `UNAUTHORIZED`，并更新描述。
    - 更名 `ERROR` 为 `REJECTED_AUTHORIZED`，并更新描述。
    - 更名 `PARAM_ERROR` 为 `ACCOUNT_DISABLED`，并更新描述。
    - 更名 `PARAM_VALIDATE_FAIL` 和 `PARAM_REQUIRED`，并更新描述。
    
    在 `GlobalMessageCode.cs` 中，更新了枚举 `GlobalMessageCode` 的多个值及其描述，新增了 `ACCOUNT_DISABLED` 和 `PARAM_REQUIRED`。

--- Com.GleekFramework.ContractSdk/Enums/GlobalMessageCode.cs
+++ Com.GleekFramework.ContractSdk/Enums/GlobalMessageCode.cs
@@ -9,15 +9,15 @@ namespace Com.GleekFramework.ContractSdk
     public enum GlobalMessageCode
     {
         /// <summary>
-        /// 操作失败
+        /// 失败
         /// </summary>
-        [Description("操作失败")]
+        [Description("失败")]
         FAIL = -1,
 
         /// <summary>
-        ///  操作成功
+        ///  成功
         /// </summary>
-        [Description("操作成功")]
+        [Description("成功")]
         SUCCESS = 200,
 
         /// <summary>
@@ -27,10 +27,16 @@ namespace Com.GleekFramework.ContractSdk
         NO_DATA_AVAILABLE = 204,
 
         /// <summary>
-        /// 登录无效
+        /// 未经授权
         /// </summary>
-        [Description("登录无效")]
-        TOKEN_INVALID = 401,
+        [Description("未经授权")]
+        UNAUTHORIZED = 401,
+
+        /// <summary>
+        /// 授权被拒绝
+        /// </summary>
+        [Description("授权被拒绝")]
+        REJECTED_AUTHORIZED = 403,
 
         /// <summary>
         ///  未知错误
@@ -39,57 +45,51 @@ namespace Com.GleekFramework.ContractSdk
         ERROR = 500,
 
         /// <summary>
-        /// 状态不可用
-        /// </summary>
-        [Description("状态不可用")]
-        STATUS_UNAVAILABLE = 1001001001,
-
-        /// <summary>
         /// 签名失败
         /// </summary>
         [Description("签名失败")]
-        SIGN_ERROR = 1001001002,
+        SIGN_ERROR = 1001001001,
 
         /// <summary>
         /// 操作频繁
         /// </summary>
         [Description("操作频繁")]
-        FREQUENT_OPERATION = 1001001003,
+        FREQUENT_OPERATION = 1001001002,
 
         /// <summary>
         /// 非法请求
         /// </summary>
         [Description("非法请求")]
-        ILLEGAL_REQUEST = 1001001004,
+        ILLEGAL_REQUEST = 1001001003,
 
         /// <summary>
-        /// 请求参数错误
+        /// 您的账号已被禁用
         /// </summary>
-        [Description("请求参数错误")]
-        PARAM_ERROR = 1001001005,
-
-        /// <summary>
-        /// 参数验证失败
-        /// </summary>
-        [Description("参数验证失败")]
-        PARAM_VALIDATE_FAIL = 1001001006,
+        [Description("您的账号已被禁用")]
+        ACCOUNT_DISABLED = 1001001004,
 
         /// <summary>
         /// 参数必填
         /// </summary>
         [Description("参数必填")]
-        PARAM_REQUIRED = 1001001007,
+        PARAM_REQUIRED = 1001001005,
+
+        /// <summary>
+        /// 请求参数错误
+        /// </summary>
+        [Description("请求参数错误")]
+        PARAM_ERROR = 1001001006,
+
+        /// <summary>
+        /// 参数验证失败
+        /// </summary>
+        [Description("参数验证失败")]
+        PARAM_VALIDATE_FAIL = 1001001007,
 
         /// <summary>
         /// 超出[1,200]长度的区间限制
         /// </summary>
         [Description("超出[1,200]长度的区间限制")]
         EXCEED_PPER_LIMIT_200 = 1001001008,
-
-        /// <summary>
-        /// 令牌权限不足
-        /// </summary>
-        [Description("令牌权限不足")]
-        INSUFFICIENT_TOKEN_PERMISSIONS = 1001001009,
     }
 }
